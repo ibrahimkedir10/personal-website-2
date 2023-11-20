fa9ae565d88d795111fb540cd5bc12e6d9e437c7
ibrahim.kedir <ibrahim.kedir@outlook.com>
2023-11-20 17:05:07 -0500

    update to style sheet

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,13 +1,14 @@
 import Proj1 from "../assets/ATM2.png";
-import Proj2 from "../assets/webapi.png";
-import Proj3 from "../assets/resumeapp.png";
-import Proj4 from "../assets/recipeapp.png";
-import Proj5 from "../assets/snakegame.png";
-import Proj6 from "../assets/harrypoter.png";
+import Proj2 from "../assets/machineLearning.jpeg";
+import Proj3 from "../assets/employeeDB.png";
+import Proj4 from "../assets/resumeapp.png";
+import Proj5 from "../assets/recipeapp.png";
+import Proj6 from "../assets/webapi.png";
+
 
 export const ProjectList = [
   {
-    name: "Wells Fargo ATM-Application",
+    name: "Easy-Web Banking Application",
     image: Proj1,
     skills: "c#, .NET. SQL,",
     link:"https://github.com/ibrahimkedir10/ATM-Application",
@@ -15,34 +16,35 @@ export const ProjectList = [
    
   },
   {
-    name: "BookStore API",
+    name: "Machine Learning",
     image: Proj2,
+    skills: "JavaScript,HTML,CSS",
+    link: "https://github.com/ibrahimkedir10/recipe-web-app",
+  },
+  {
+    name: "Employee Database System",
+    image: Proj3,
     skills: "ASP.NET Core 5.0,c#, MongoDB",
-    link:"https://github.com/ibrahimkedir10/BookStoreAPIGIT",
+    link:"https://github.com/ibrahimkedir10/Employee-DB",
   },
   {
     name: "Roast My Resume",
-    image: Proj3,
+    image: Proj4,
     skills: "React,ASP.NET, C# ,MongoDB,",
     link:"https://github.com/ibrahimkedir10/RoastMyResume.1",
   },
   {
     name: "Recipe website API",
-    image: Proj4,
+    image: Proj5,
     skills:"JavaScript, MealDB API, HTML,CSS",
     link: "https://github.com/ibrahimkedir10/recipe-web-app",
   },
   {
-    name: "Snake Game",
-    image: Proj5,
-    skills: "JavaScript,HTML,CSS",
-    link: "https://github.com/ibrahimkedir10/recipe-web-app",
-  },
-  {
-    name: "Harry Poter Movie Generator",
+    name: "BookStore API",
     image: Proj6,
-    skills: "React Native,JavaScript,HTML,CSS",
-    link: "https://github.com/ibrahimkedir10/harryPoter-movie-trailer-generator-web-app"
-  },
+    skills: "ASP.NET Core 5.0,c#, MongoDB",
+    link:"https://github.com/ibrahimkedir10/BookStoreAPIGIT",
+  }
+ ,
  
 ];
